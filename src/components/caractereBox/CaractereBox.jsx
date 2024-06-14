08a640494c720eacf8b9f7111775affcecff84a1
Wendel <wendel.isaac12@gmail.com>
2024-06-14 14:17:54 -0300

    feat: componente para exibir caractere

---
+++ src/components/caractereBox/CaractereBox.jsx
@@ -0,0 +1,11 @@
+import "./caractereBox.scss"
+
+const CaractereBox = ({caractere}) => {
+    return ( 
+        <div className="caractere">
+            {caractere}
+        </div>
+     );
+}
+ 
+export default CaractereBox;
