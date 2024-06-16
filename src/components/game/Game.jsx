69adb9be78081c594aaec9dabc1d7af2f796d6f1
Wendel <wendel.isaac12@gmail.com>
2024-06-16 03:47:49 -0300

    feat: configurando as funções para aparecer determinado componente

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,23 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Caracteres from "../caracteres/Caracteres";
 import Menu from "../menu/Menu";
 import { AnimatePresence } from 'framer-motion';
+import GameOver from "../gameOver/GameOver";
 
 const Game = () => {
 
     const [showMenu, setShowMenu] = useState(true);
+    const [showGameOver, setShowGameOver] = useState(false)
+    const [player, setPlayer] = useState("visitante")
+    const [pontos, setPontos] = useState(0)
 
-    const handleStart = () => {
-        setShowMenu(false);
+    const handleMenu = () =>{
+        setShowMenu(true)
+        setShowGameOver(false)
     }
-    
+
+    const handleStart = (data) => {
+        setShowMenu(false);
+        setPlayer(data)
+    }
+
+    const handleEnd = (end) =>{
+        setShowGameOver(end.end)
+        setPontos(end.pontos)
+    }
+
+    const handleRestart = () => {
+        setShowGameOver(false)
+        setShowMenu(false)
+        setPontos(0)
+    }
     return ( 
         <>
             <AnimatePresence>
-                {showMenu ? (
+            {showMenu ? (
                     <Menu key="menu" onStart={handleStart} />
+                ) : showGameOver ? (
+                    <GameOver key="gameover" onRestart={handleRestart} onMenu={handleMenu} pontos={pontos} player={player}/>
                 ) : (
-                    <Caracteres key="caracteres" />
+                    <Caracteres key="caracteres" onData={handleEnd} />
                 )}
             </AnimatePresence>
         </>
