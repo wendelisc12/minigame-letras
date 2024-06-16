4fee1606e06189c4b17e0a731c78a507d3afabfa
Wendel <wendel.isaac12@gmail.com>
2024-06-16 03:47:04 -0300

    feat: adicionando logica de fim de jogo, pontuação e nome

--- src/components/caracteres/Caracteres.jsx
+++ src/components/caracteres/Caracteres.jsx
@@ -1,15 +1,19 @@
 import { useEffect, useState } from "react";
 import CaractereBox from "../caractereBox/CaractereBox";
-import {motion} from "framer-motion"
+import { motion } from "framer-motion"
 import "./caracteres.scss"
+import TempoProgresso from "../tempoProgresso/TempoProgresso";
 
-const Caracteres = ({onData}) => {
-    
+const Caracteres = ({ onData }) => {
+
     const [caracteresEscolhidos, setCaracteresEscolhidos] = useState([])
     const [caracteresCorretos, setCaracteresCorretos] = useState([])
     const [indexCaracter, setIndexCaracter] = useState(0)
+    const [pontos, setPontos] = useState(0)
+    const [endTime, setEndTime] = useState(false)
+    const [endGame, setEndGame] = useState(false)
     const caracteres = []
-    
+
     for (let i = 48; i <= 57; i++) {
         caracteres.push(String.fromCharCode(i));
     }
@@ -17,55 +21,95 @@ const Caracteres = ({onData}) => {
         caracteres.push(String.fromCharCode(i));
     }
 
-    
     const gerarCaracteres = () => {
         const escolhidos = []
-        for(let i =0; i<6; i++){
+        for (let i = 0; i < 6; i++) {
             escolhidos.push(caracteres[Math.floor(Math.random() * 36)])
         }
 
         return escolhidos
     }
 
-    const teclaPressionada = (e) =>{
-        if(e.key == caracteresEscolhidos[indexCaracter]){
-            setCaracteresCorretos(prevState => [...prevState, indexCaracter])
-            setIndexCaracter(prevIndex => prevIndex + 1);
-        }else{
-            iniciarGame()
+    const teclaPressionada = (e) => {
+        if (/^[a-zA-Z0-9]$/.test(e.key)) {
+            if (e.key == caracteresEscolhidos[indexCaracter]) {
+                setCaracteresCorretos(prevState => [...prevState, indexCaracter])
+                setIndexCaracter(prevIndex => prevIndex + 1);
+                setPontos(prevIndex => prevIndex + 1)
+            } else {
+                setEndGame(true)
+            }
         }
     }
 
-    const iniciarGame =()=>{
+    const iniciarGame = () => {
         const caracteresGerados = gerarCaracteres();
         setCaracteresEscolhidos(caracteresGerados);
         setCaracteresCorretos([]);
         setIndexCaracter(0);
+        setEndGame(false);
     }
 
-    useEffect(()=>{
-        iniciarGame()
-    },[])
+    const fimGame = (isEnd) => {
+        setEndTime(isEnd)
+    }
 
-    useEffect(()=>{        
+    useEffect(() => {
+        if (endGame || endTime) {
+            onData({end: true, pontos: pontos});
+        }
+    }, [endGame, endTime, onData]);
+
+    useEffect(() => {
+        if (caracteresCorretos.length == 6) {
+            iniciarGame()
+        }
+    }, [caracteresCorretos])
+
+    useEffect(() => {
+        if (!endGame) {
+            iniciarGame()
+        }
+    }, [])
+
+    useEffect(() => {
         window.addEventListener('keydown', teclaPressionada)
 
         return () => {
-          window.removeEventListener('keydown', teclaPressionada)
+            window.removeEventListener('keydown', teclaPressionada)
         };
-    }, [indexCaracter,caracteresEscolhidos])
+    }, [indexCaracter, caracteresEscolhidos])
 
-    return(
-        <motion.div 
-        initial={{ y: 600, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.6, delay: 0.3 }}
-        className="caracteres_row">
-            {caracteresEscolhidos.map((caracter, index) => (
-                <CaractereBox key={index} caractere={caracter} certo={caracteresCorretos.includes(index)}/>
-            ))}
+    return (
+        <motion.div
+            initial={{ y: 600, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.7, delay: 0.3 }}
+
+            className="caracteres">
+
+                <div className="caracteres_pontos">
+                    <h1>{pontos}</h1>
+                </div>
+
+            <div className="caracteres_box">
+
+                <div className="caracteres_box_row">
+                    {caracteresEscolhidos.map((caracter, index) => (
+                        <motion.div
+                            key={index}
+                            initial={{ y: -200, opacity: 0 }}
+                            animate={{ y: 0, opacity: 1 }}
+                            transition={{ duration: 0.7, delay: index * 0.180 }}
+                        >
+                            <CaractereBox caractere={caracter} certo={caracteresCorretos.includes(index)} />
+                        </motion.div>
+                    ))}
+                </div>
+                <TempoProgresso onEnd={fimGame} />
+            </div>
         </motion.div>
     )
 }
- 
+
 export default Caracteres;
